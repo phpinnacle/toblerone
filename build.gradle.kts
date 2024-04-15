708fe1c2892b6aa88b71a8a4d3294e6c6b7e2805
Anton Shabouta <zloyusr@gmail.com>
2024-04-16 02:12:29 +0300

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
  * User Manual available at https://docs.gradle.org/7.4/userguide/building_java_projects.html
  */
 
-group = "at.willhaben.kafka.connect.transforms.jslt"
+group = "org.phpinnacle.toblerone"
 version = System.getenv("VERSION") ?: "1.0.0"
 
 val javaVersion = 11
@@ -15,7 +15,7 @@ val artifactoryPassword =
     project.properties.getOrDefault("artifactory_password", System.getenv("ARTIFACTORY_PWD")).toString()
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.9.22"
     idea // Generates files that are used by IntelliJ IDEA, thus making it possible to open the project from IDEA
     `java-library` // Apply the java-library plugin for API and implementation separation.
     `maven-publish`
@@ -27,11 +27,10 @@ repositories {
 }
 
 dependencies {
-    val kafkaConnectVersion = "3.2.+"
+    val kafkaConnectVersion = "3.7.+"
     val junitVersion = "5.8.2"
 
-    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom")) // Align versions of all Kotlin components
-    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // Use the Kotlin JDK 8 standard library.
+    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
 
     implementation("org.apache.kafka:connect-api:$kafkaConnectVersion")
     implementation("org.apache.kafka:connect-json:$kafkaConnectVersion")
@@ -44,6 +43,22 @@ dependencies {
 kotlin {
     jvmToolchain {
         (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(javaVersion))
+    }
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to "org.phpinnacle.toblerone")) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
     }
 }
 
